60c82c6344d00f1f414d1a41af073496eb53e39f
xiaoqiu.duan <xiaoqiu.duan@shopee.com>
2026-04-07 15:38:34 +0800

    feat:link

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -10,6 +10,9 @@ export function SiteFooter() {
   return (
     <footer className="border-t py-10 md:py-6">
       <div className="container-fluid">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <a href="https://www.crxsoso.com" target="_blank">Crx搜搜</a>
+        </div>
       </div>
     </footer>
   );
